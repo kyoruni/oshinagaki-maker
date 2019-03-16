5337b9a033c8a04655cf9b5015b332afa120e0a9
miyuki-muneishi <kyoruni@gmail.com>
2019-03-16 14:07:45 +0000

    add #1 ユーザーログイン・ログアウト機能追加

--- resources/lang/ja/auth.php
+++ resources/lang/ja/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed'   => 'メールアドレスまたはパスワードが違います。',
+    'throttle' => 'ログイン入力回数の制限に達したためロックされています。:seconds 秒後にやり直してください。',
 
 ];
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center">
+        <h1>ログイン</h1>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6 offset-sm-3">
+
+            {!! Form::open(['route' => 'login.post']) !!}
+                <div class="form-group">
+                    {!! Form::label('email', 'メールアドレス') !!}
+                    {!! Form::label('email', '(半角)', ['class' => 'text-muted small']) !!}
+                    {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('password', 'パスワード') !!}
+                    {!! Form::label('password', '(6文字以上 半角英数のみ)', ['class' => 'text-muted small']) !!}
+                    {!! Form::password('password', ['class' => 'form-control']) !!}
+                </div>
+
+                {!! Form::submit('ログイン', ['class' => 'btn btn-primary btn-block']) !!}
+            {!! Form::close() !!}
+
+            <p class="mt-2">新規登録は {!! link_to_route('signup.get', 'こちら') !!}</p>
+        </div>
+    </div>
+@endsection
