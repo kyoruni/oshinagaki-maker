81eb7a502dd202f1a51042e731d7fb370708061d
miyuki-muneishi <kyoruni@gmail.com>
2019-04-07 13:34:38 +0000

    update #7 画像登録画面レイアウト作成

--- resources/views/image/upload.blade.php
+++ resources/views/image/upload.blade.php
@@ -1,28 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="text-center">
-        <h1>画像登録</h1>
-    </div>
-
-    <div class="row">
-        <div class="col-sm-6 offset-sm-3">
-            {!! Form::model($image, ['route' => ['image.store', $image->id], 'method' => 'post', 'files' => true]) !!}
-                <div class="form-group">
-                    {!! Form::label('photo', '画像') !!}
-                    {!! Form::file('photo', ['class' => 'form-control']) !!}
-
-                    {!! Form::label('title', 'タイトル') !!}
-                    {!! Form::text ('title', old('title'), ['class' => 'form-control']) !!}
-
-                    {!! Form::label('price', '価格') !!}
-                    {!! Form::text ('price', old('price'), ['class' => 'form-control']) !!}
-
-                    {!! Form::label('comment', 'コメント') !!}
-                    {!! Form::text ('comment', old('comment'), ['class' => 'form-control']) !!}
+        <div class="row">
+            <div class="col-sm-4">
+                @include('layouts.menu')
+            </div>
+            <div class="col-sm-8">
+                <div class="text-center">
+                    <h1>画像登録</h1>
                 </div>
-                {!! Form::submit('登録', ['class' => 'btn btn-primary mb-4', 'onclick' => 'return conf_message();']) !!}
-            {!! Form::close() !!}
+                {!! Form::model($image, ['route' => ['image.store', $image->id], 'method' => 'post', 'files' => true]) !!}
+                    <div class="form-group">
+                        {!! Form::label('photo', '画像') !!}
+                        {!! Form::file('photo', ['class' => 'form-control']) !!}
+
+                        {!! Form::label('title', 'タイトル') !!}
+                        {!! Form::text ('title', old('title'), ['class' => 'form-control']) !!}
+
+                        {!! Form::label('price', '価格') !!}
+                        {!! Form::text ('price', old('price'), ['class' => 'form-control']) !!}
+
+                        {!! Form::label('comment', 'コメント') !!}
+                        {!! Form::text ('comment', old('comment'), ['class' => 'form-control']) !!}
+                    </div>
+                    {!! Form::submit('登録', ['class' => 'btn btn-primary mb-4', 'onclick' => 'return conf_message();']) !!}
+                {!! Form::close() !!}
+            </div>
         </div>
-    </div>
 @endsection
