f938cf3754425d43f281522ce70b4c1dea9d2d63
miyuki-muneishi <kyoruni@gmail.com>
2019-04-10 16:05:32 +0000

    update #14 フォームに入力した値でプレビューできるように変更

--- public/js/disp_image.js
+++ public/js/disp_image.js
@@ -1,11 +1,22 @@
-function draw_image(canvas, mode, width, height, path, title, comment, price)
+function draw_image(canvas, mode, width, height, path)
 {
-      var image = new Image();
-      image.src = path;
+    var image = new Image();
+    image.src = path;
 
       // 画像(image)の読み込みが完了したら、描画処理に入る
       image.onload = function(){
-        var ctx = document.getElementById(canvas).getContext("2d");
+        var text = new Object();
+        text.title   = '';
+        text.price   = '';
+        text.comment = '';
+
+        // プレビューボタン押下時、入力されたテキストを取得
+        if (mode == 'preview'){
+        text = get_text();
+        }
+
+        var ctx  = document.getElementById(canvas).getContext("2d");
+        ctx.clearRect(0,0,width,height);
 
         // 読み込んだ画像を表示する
         // 引数4個：元画像を縮小して表示 image, x, y, width, height
@@ -17,16 +28,26 @@ function draw_image(canvas, mode, width, height, path, title, comment, price)
         ctx.font         = "30px 'ＭＳ ゴシック'";
 
         // タイトルのテキストを描画 text, x, y
-        ctx.fillText(title, 10, 10);
+        ctx.fillText(text.title, 10, 10);
 
         // コメントのテキストを描画
         ctx.textAlign = "right";
-        ctx.fillText(comment, width-20, height-50);
+        ctx.fillText(text.comment, width-20, height-50);
 
         // 価格のテキストを描画
         if (mode == "preview"){
             ctx.font = "24px 'ＭＳ ゴシック'";
-            ctx.fillText("¥" + price, width-20, height-90);
+            ctx.fillText("¥" + text.price, width-20, height-90);
         }
       };
+}
+
+function get_text()
+{
+    var text = new Object();
+
+    text.title   = document.forms.image_form.textbox_title.value;
+    text.price   = document.forms.image_form.textbox_price.value;
+    text.comment = document.forms.image_form.textbox_comment.value;
+    return text;
 }
--- resources/views/image/edit.blade.php
+++ resources/views/image/edit.blade.php
@@ -9,32 +9,30 @@
             <div class="text-center">
                 <h1>画像情報変更</h1>
  
-<!--                <img src="{{ asset($image->path) }}" width=300 height=300> -->
-<!--                <img src="{{ asset($image->path) }}" width=300 height=300> -->
                 <canvas id="cv1" width="300" height="300" style="border:1px solid #000;"></canvas>
                 <canvas id="cv2" width="300" height="300" style="border:1px solid #000;"></canvas>
                     <script>
-                        window.onload = draw_image( "cv1", "original", 300, 300, "{{ asset($image->path) }}", "", "", "");
+                        window.onload = draw_image( "cv1", "original", 300, 300, "{{ asset($image->path) }}" );
+                        window.onload = draw_image( "cv2", "preview",  300, 300, "{{ asset($image->path) }}" );
                     </script>
             </div>
-            {!! Form::model($image, ['route' => ['image.update', $image->id], 'method' => 'put', 'files' => true]) !!}
+            {!! Form::model($image, ['route' => ['image.update', $image->id], 'method' => 'put', 'files' => true, 'name' => 'image_form']) !!}
                 <div class="form-group">
                     {!! Form::label('photo', '画像') !!}
                     {!! Form::file('photo', ['class' => 'form-control']) !!}
 
                     {!! Form::label('title', 'タイトル') !!}
-                    {!! Form::text ('title', old('title'), ['class' => 'form-control']) !!}
+                    {!! Form::text ('title', old('title'), ['class' => 'form-control', 'name' => 'textbox_title',]) !!}
 
                     {!! Form::label('price', '価格') !!}
-                    {!! Form::text ('price', old('price'), ['class' => 'form-control']) !!}
+                    {!! Form::text ('price', old('price'), ['class' => 'form-control', 'name' => 'textbox_price',]) !!}
 
                     {!! Form::label('comment', 'コメント') !!}
-                    {!! Form::text ('comment', old('comment'), ['class' => 'form-control']) !!}
+                    {!! Form::text ('comment', old('comment'), ['class' => 'form-control', 'name' => 'textbox_comment',]) !!}
                 </div>
                 {!! Form::submit('登録', ['class' => 'btn btn-primary mb-4', 'onclick' => 'return conf_message();']) !!}
                 <input type="button" value="preview" class="btn btn-info mb-4" onclick="draw_image('cv2', 'preview', 300, 300, '{{ asset($image->path) }}', '{!! $image->title !!}', '{!! $image->comment !!}', '{!! $image->price !!}');">
             {!! Form::close() !!}
-        </div>
         </div>
     </div>
 @endsection
